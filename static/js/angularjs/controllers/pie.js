3019736e3c4f7fd5b9bdd1d6f83490e0a1075fc6
Lewis Barber <lewis.barber@capco.com>
2014-04-23 07:32:50 +0100

    Pie chart fix with Angular

--- static/js/angularjs/controllers/pie.js
+++ static/js/angularjs/controllers/pie.js
@@ -5,11 +5,36 @@ controllers.controller('PieController', ['$scope', '$route', '$http', '$rootScop
     $scope.tweets = [];
     $scope.hashtag = "";
 
+    $scope.doesGroupExist = function(tweetGroups, group) {
+        for(var j=0; j<tweetGroups.length; j++) {
+            if(tweetGroups[j].country == group) {
+                return true;
+            }
+        }
+        return false;
+    };
+
+    $scope.addGroup = function(tweetGroups, group) {
+        tweetGroups.push({
+            country: group,
+            count: 1
+        });
+    };
+
+    $scope.incrementGroup = function(tweetGroups, group) {
+        for(var j=0; j<tweetGroups.length; j++) {
+            if(tweetGroups[j].country == group) {
+                tweetGroups[j].count++;
+            }
+        }
+    };
+
     $scope.getTweets = function() {
 
-        localStorage.setItem("tweets", null);
-
         $scope.tweets = [];
+
+        $('#pie-chart').hide();
+        $('.tweets-table').hide();
 
         var hashtag = "";
         if($scope.hashtag == "") {
@@ -31,7 +56,52 @@ controllers.controller('PieController', ['$scope', '$route', '$http', '$rootScop
                 }
             }
 
-            localStorage.setItem("tweets", JSON.stringify($scope.tweets));
+            var tweetGroups = [];
+
+            for(var i=0; i<$scope.tweets.length; i++) {
+                var result = $scope.doesGroupExist(tweetGroups, $scope.tweets[i].user.time_zone);
+                if(!result) {
+                    $scope.addGroup(tweetGroups, $scope.tweets[i].user.time_zone);
+                } else {
+                    $scope.incrementGroup(tweetGroups, $scope.tweets[i].user.time_zone);
+                }
+            }
+
+            var data = [];
+
+            for(var x=0; x<tweetGroups.length; x++) {
+                data.push([tweetGroups[x].country, tweetGroups[x].count]);
+            }
+
+            $('#pie-chart').fadeIn(400);
+            $('.tweets-table').fadeIn(400);
+
+            $.jqplot ('pie-chart', [data],
+                {
+                    height:400,
+                    seriesDefaults: {
+                        renderer: $.jqplot.PieRenderer,
+                        rendererOptions: {
+                            showDataLabels: true
+                        }
+                    },
+                    grid: {
+                        background: "#F9F9F9",
+                        borderColor:"#CCCCCC",
+                        borderWidth:1,
+                        shadow:false
+                    },
+                    highlighter: {
+                        show: true,
+                        showTooltip: true,
+                        formatString:'%s',
+                        tooltipLocation: 'ne',
+                        useAxesFormatters: false
+                    },
+                    legend: { show:true, location: 'e' },
+                    seriesColors: ["#F7977A", "#F9AD81", "#FDC68A", "#FFF79A", "#C4DF9B", "#A2D39C", "#82CA9D", "#7BCDC8", "#6ECFF6", "#7EA7D8", "#8493CA", "#8882BE", "#A187BE", "#BC8DBF", "#F49AC2", "#F6989D"]
+                }
+            ).replot();
 
             console.log($scope.tweets);
 
